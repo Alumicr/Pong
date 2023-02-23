0dad727b581341232c67c32631a5ac54c353f693
Morgan Harvey <108724899+Alumicr@users.noreply.github.com>
2023-02-23 20:20:51 +0000

    Added walls, ball physics, added a paddle

--- script.js
+++ script.js
@@ -5,12 +5,36 @@
 
 function setup() {
   cnv = new Canvas(windowWidth, windowHeight);
+  ball = new Sprite(700, 200, 80, "d");
+  ball.shapeColor = color("white");
+  ball.vel.x = -5;
+  ball.vel.y = 0;
+  ball.bounciness = 0;
+
+  paddle = new Sprite (200, 200, 10, 90, 'k');
+  paddle.shapeColor = color("white");
+
+
+  walls();
+
+
 }
 
 
 
+function walls(){
+  wallLH = new Sprite(0, height / 2, 8, height, 'k');
+  wallRH = new Sprite(width, height / 2, 8, height, 'k');
+  wallTop = new Sprite(0, 8, width * 2, 8, 'k');
+  wallBot = new Sprite(width, height + 8, width * 2, 8, 'k');
+  wallLH.shapeColor = color("red");
+  wallRH.shapeColor = color("white");
+  wallTop.shapeColor = color("white");
+  wallBot.shapeColor = color("white");
+}
+
 
 
 function draw() {
-  background("grey");
+  background("black");
 }
