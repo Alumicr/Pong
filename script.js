3640aea353f20cd23efe7335533b69c7af8103b3
Morgan Harvey <108724899+Alumicr@users.noreply.github.com>
2023-02-23 20:33:11 +0000

    Added movement to paddle

--- script.js
+++ script.js
@@ -4,25 +4,52 @@
 
 
 function setup() {
+
+  // creats canvas and ball
   cnv = new Canvas(windowWidth, windowHeight);
-  ball = new Sprite(700, 200, 80, "d");
+  ball = new Sprite(800, 200, 80, "d");
   ball.shapeColor = color("white");
   ball.vel.x = -5;
   ball.vel.y = 0;
   ball.bounciness = 0;
 
-  paddle = new Sprite (200, 200, 10, 90, 'k');
+  // makes the paddle
+  paddle = new Sprite(200, 500, 10, 90, 'k');
   paddle.shapeColor = color("white");
-
-
+  paddle.vel.y = 0;
   walls();
 
+
+  document.addEventListener("keydown", function(event) {
+
+    if (event.code == 'ArrowUp') {
+      paddle.vel.y = -5;
+    }
+    else if (event.code == 'ArrowDown') {
+      paddle.vel.y = 5;
+
+    }
+  })
+
+
+  document.addEventListener("keyup", function(event) {
+
+    if (event.code === 'ArrowUp') {
+      paddle.vel.y = 0;
+    }
+
+    else if (event.code == 'ArrowDown') {
+      paddle.vel.y = 0;
+
+    }
+  })
 
 }
 
 
 
-function walls(){
+function walls() {
+  // Function creates walls and colours them
   wallLH = new Sprite(0, height / 2, 8, height, 'k');
   wallRH = new Sprite(width, height / 2, 8, height, 'k');
   wallTop = new Sprite(0, 8, width * 2, 8, 'k');
@@ -36,5 +63,6 @@ function walls(){
 
 
 function draw() {
+  // background color
   background("black");
 }
