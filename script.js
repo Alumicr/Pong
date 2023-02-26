216cf2baac6157254f9b86c46d6c1edbc00bbf94
Morgan Harvey <108724899+Alumicr@users.noreply.github.com>
2023-02-26 21:43:44 +0000

    Touched up ball and paddle movement

--- script.js
+++ script.js
@@ -7,29 +7,28 @@ function setup() {
 
   // creats canvas and ball
   cnv = new Canvas(windowWidth, windowHeight);
-  ball = new Sprite(800, 200, 50, "d");
+  ball = new Sprite(1200, 200, 50, "d");
   ball.shapeColor = color("white");
-  ball.vel.x = -15;
-  ball.vel.y = 1;
+  ball.vel.x = -5;
+  ball.vel.y = 9;
   ball.bounciness = 1;
-  ball.friction = 10;
+  ball.friction = 0;
   ball.drag = 0;
 
   // makes the paddle
   paddle = new Sprite(200, 500, 10, 90, 'k');
   paddle.shapeColor = color("white");
   paddle.vel.y = 0;
-  paddle.bounciness = 1;
   walls();
 
   // paddle movement
   document.addEventListener("keydown", function(event) {
 
     if (event.code == 'ArrowUp') {
-      paddle.vel.y = -5;
+      paddle.vel.y = -6;
     }
     else if (event.code == 'ArrowDown') {
-      paddle.vel.y = 5;
+      paddle.vel.y = 6;
 
     }
   })
@@ -53,14 +52,13 @@ function setup() {
 
 function walls() {
   // Function creates walls and colours them
-  wallLH = new Sprite(0, height / 2, 8, height, 'k');
   wallRH = new Sprite(width, height / 2, 8, height, 'k');
   wallTop = new Sprite(0, 8, width * 2, 8, 'k');
   wallBot = new Sprite(width, height + 8, width * 2, 8, 'k');
-  wallLH.shapeColor = color("red");
   wallRH.shapeColor = color("white");
   wallTop.shapeColor = color("white");
   wallBot.shapeColor = color("white");
+  wallRH.bounciness = 1;
 }
 
 
