037456699626017b32baf4a4dfea48f1a9b62bb2
Morgan Harvey <108724899+Alumicr@users.noreply.github.com>
2023-04-07 06:55:48 +0000

    Rearnged wall color and sizes, added ws movement

--- script.js
+++ script.js
@@ -1,64 +1,57 @@
 //  start of code 
 
-
-
-
 function setup() {
 
   // creats canvas and ball
-  cnv = new Canvas(windowWidth, windowHeight);
+  cnv = new Canvas(windowWidth, windowHeight - 10);
   ball = new Sprite(1200, 200, 50, "d");
-  ball.shapeColor = color("white");
+  ball.color = color("white");
   ball.vel.x = -5;
   ball.vel.y = 9;
   ball.bounciness = 1;
   ball.friction = 0;
   ball.drag = 0;
-
+  wallGroup = new Group();
   // makes the paddle
   paddle = new Sprite(200, 500, 10, 90, 'k');
-  paddle.shapeColor = color("white");
+  paddle.color = color("white");
   paddle.vel.y = 0;
   walls();
 
-  // paddle movement
+  // paddle movementa
   document.addEventListener("keydown", function(event) {
 
-    if (event.code == 'ArrowUp') {
-      paddle.vel.y = -6;
+    if (event.code == 'ArrowUp'  ||  event.code == "KeyW") {
+      paddle.vel.y = -8;
     }
-    else if (event.code == 'ArrowDown') {
-      paddle.vel.y = 6;
+    else if (event.code == 'ArrowDown' ||  event.code == "KeyS") {
+      paddle.vel.y = 8;
 
     }
   })
-
 
   document.addEventListener("keyup", function(event) {
-
-    if (event.code === 'ArrowUp') {
+    if (event.code === 'ArrowUp' ||  event.code == "KeyS" ||  event.code == "KeyW" || event.code == 'ArrowDown') {
       paddle.vel.y = 0;
-    }
-
-    else if (event.code == 'ArrowDown') {
-      paddle.vel.y = 0;
-
     }
   })
-
 }
-
-
 
 function walls() {
   // Function creates walls and colours them
   wallRH = new Sprite(width, height / 2, 8, height, 'k');
-  wallTop = new Sprite(0, 8, width * 2, 8, 'k');
-  wallBot = new Sprite(width, height + 8, width * 2, 8, 'k');
-  wallRH.shapeColor = color("white");
-  wallTop.shapeColor = color("white");
-  wallBot.shapeColor = color("white");
+  wallRH.color = color('white');
   wallRH.bounciness = 1;
+  wallLH = new Sprite(0, height / 2, 8, height, 'k');
+  wallLH.color = color('red');
+  wallTop = new Sprite(width / 2, 0, width, 8, 'k');
+  wallTop.color = color('white');
+  wallBot = new Sprite(width / 2, height + 4, width * 2, 8, 'k');
+  wallBot.color = color('white');
+  wallGroup.add(wallRH);
+  wallGroup.add(wallLH);
+  wallGroup.add(wallTop);
+  wallGroup.add(wallBot);
 }
 
 
@@ -67,3 +60,4 @@ function draw() {
   // background color
   background("black");
 }
+
